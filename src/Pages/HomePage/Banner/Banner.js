08ced41f191881950e808cd5133c73eca273c1b7
AryanPiash <aryanpiash420@gmail.com>
2022-04-18 02:39:06 +0600

    added google fonts and changes carousel caption

--- src/Pages/HomePage/Banner/Banner.js
+++ src/Pages/HomePage/Banner/Banner.js
@@ -3,6 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../../images/banner/camera.png'
 import banner2 from '../../../images/banner/adventure.jpg'
 import banner3 from '../../../images/banner/city.png'
+import './Banner.css';
 
 
 const Banner = () => {
@@ -15,9 +16,9 @@ const Banner = () => {
                     src={banner1}
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <Carousel.Caption className='carousel-caption'>
+                    <h3>The Beauty of the Nature</h3>
+                    <p>The beauty of nature can have a profound effect upon our senses, those gateways from the outer world to the inner, whether it results in disbelief in its very existence as Emerson notes, or feelings such as awe, wonder, or amazement.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={2000}>
@@ -27,9 +28,9 @@ const Banner = () => {
                     alt="Second slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <Carousel.Caption className='carousel-caption'>
+                    <h3>Providing the best shoots of Nature.</h3>
+                    <p>It’s often said that “the best camera is the one you have with you” – you don’t need a high-end professional camera to get a great shot, and even with the best equipment there’s no disguising poor composition and poor lighting. A thoughtfully-composed smartphone image will almost always beat a hasty snap on an expensive camera.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={3000}>
@@ -39,11 +40,9 @@ const Banner = () => {
                     alt="Third slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                <Carousel.Caption className='carousel-caption'>
+                    <h3>Night mood of city also amazing nature feel.</h3>
+                    <p>Slow movements such as the ripples of water or clouds moving across the sky place effortless demands on our working memory but enough to distract us from spiralling rumination, self-blame and hopelessness.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
